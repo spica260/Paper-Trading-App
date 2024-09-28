99dcb8b368e72a39e6c4b7735156e70e19c48bdd
Maria P <mariagpirela@gmail.com>
2024-09-28 10:05:14 -0500

    Add models
    Add customer and product models.

--- trading/trade/models.py
+++ trading/trade/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    buyer = models.CharField(max_length=50)
+    credit = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+
+    def __str__(self):
+        return self.buyer
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+
+    def __str__(self):
+        return self.name

f5747ae40500eb8688c34e933b8bc88098391c8c
Maria P <mariagpirela@gmail.com>
2024-09-28 09:46:27 -0500

    Create views and url paths
    URL paths for project and app.
    Media static files added to settings.
    Views added.

--- trading/trade/views.py
+++ trading/trade/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def product(request, pk):
+    pass
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
+
+def register_user(request):
+    pass
+
